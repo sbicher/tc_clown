9153a69f85bbf67b61fb7fc3416c164af677bce2
Stefan Bicher <stefan.bicher@data-experts.de>
2020-02-08 22:12:24 +0100

    Internationalisation added

--- src/main/java/de/sbicher/tc_clown/TcModule.java
+++ src/main/java/de/sbicher/tc_clown/TcModule.java
@@ -1,6 +1,8 @@
 package de.sbicher.tc_clown;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import java.util.Locale;
 
 /**
  * Module for the guice dependency injection
@@ -10,5 +12,14 @@ public class TcModule extends AbstractModule {
     @Override
     protected void configure() {
         super.configure();
+    }
+
+    /**
+     * Provides the used locale settings for the application for internationalization
+     * @return Used Localsettings
+     */
+    @Provides
+    private Locale getUsedLocale() {
+        return new Locale("de","DE");
     }
 }
--- src/main/java/de/sbicher/tc_clown/i18n/TcNames.java
+++ src/main/java/de/sbicher/tc_clown/i18n/TcNames.java
@@ -1,11 +1,32 @@
 package de.sbicher.tc_clown.i18n;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javax.inject.Inject;
+
 /**
  * Class for delivering the names, titles, descriptions, etc. of components
  */
 public class TcNames {
 
+
+    private final ResourceBundle bundle;
+
+    /**
+     * Constructor
+     * @param usedLocale Locale to use for i18n
+     */
+    @Inject
+    public TcNames(Locale usedLocale) {
+        bundle = ResourceBundle.getBundle("MessageBundles",usedLocale);
+    }
+
+    /**
+     * Returns the internationalized String for the given identifier
+     * @param identifier Identifier, for which a string is searched
+     * @return Internationalized string
+     */
     public String get(String identifier) {
-        return identifier;
+        return bundle.getString(identifier);
     }
 }
