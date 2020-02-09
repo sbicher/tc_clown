a1395c84ae31bd88e3ad4c34e97fe76702b5568a
Stefan Bicher <stefan.bicher@data-experts.de>
2020-02-09 15:19:38 +0100

    Sorting added

--- src/main/java/de/sbicher/tc_clown/ui/TcFileTableModel.java
+++ src/main/java/de/sbicher/tc_clown/ui/TcFileTableModel.java
@@ -4,6 +4,8 @@ import de.sbicher.tc_clown.i18n.TcNames;
 import de.sbicher.tc_clown.model.TcFileInfo;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
@@ -19,6 +21,32 @@ public class TcFileTableModel extends AbstractTableModel {
     private static final int COL_NAME = 0;
     private static final int COL_SIZE = 1;
     private static final int COL_LAST_MODIFIED = 2;
+
+    private int sortedColumn = COL_NAME;
+
+    private boolean sortAscending = true;
+
+    private final Comparator nameComparator = new Comparator<TcFileInfo>() {
+        @Override
+        public int compare(TcFileInfo o1, TcFileInfo o2) {
+            return o1.getName().compareToIgnoreCase(o2.getName());
+        }
+    };
+
+
+    private final Comparator sizeComparator = new Comparator<TcFileInfo>() {
+        @Override
+        public int compare(TcFileInfo o1, TcFileInfo o2) {
+            return Long.compare(o1.getSize().getByteCount(),o2.getSize().getByteCount());
+        }
+    };
+
+    private final Comparator lastModifiedComparator = new Comparator<TcFileInfo>() {
+        @Override
+        public int compare(TcFileInfo o1, TcFileInfo o2) {
+            return o1.getLastModified().getDate().compareTo(o2.getLastModified().getDate());
+        }
+    };
 
     public TcFileTableModel(TcNames names) {
         this.names = names;
@@ -44,6 +72,8 @@ public class TcFileTableModel extends AbstractTableModel {
         for (File f:files) {
             this.files.add(new TcFileInfo(f));
         }
+
+        sort();
         this.fireTableDataChanged();
     }
 
@@ -76,6 +106,43 @@ public class TcFileTableModel extends AbstractTableModel {
                 throw new IllegalArgumentException("unknown column index: " + columnIndex);
         }
     }
+
+    public void setSortedColumn(int sortedColumn, boolean ascending) {
+        this.sortedColumn = sortedColumn;
+        this.sortAscending = ascending;
+
+        sort();
+    }
+
+    /**
+     * Sorts the model with the current sort settings (column, direction)
+     */
+    private void sort() {
+        Comparator<TcFileInfo> comparator = null;
+        switch (this.sortedColumn) {
+            case COL_NAME:
+                comparator = this.nameComparator;
+                break;
+            case COL_SIZE:
+                comparator  = this.sizeComparator;
+                break;
+            case COL_LAST_MODIFIED:
+                comparator = this.lastModifiedComparator;
+                break;
+            default:
+                throw new IllegalArgumentException("can't sort by column " + sortedColumn);
+
+        }
+
+        this.files.sort(comparator);
+        if (!sortAscending) {
+            Collections.reverse(this.files);
+        }
+
+        fireTableDataChanged();
+    }
+
+
 
     /**
      * Setds the displayed directory in this model
